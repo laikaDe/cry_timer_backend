2ddceb53615ca82892add8a5e2644a43c6467e6a
Laika Deunert <laikadeunert@MacBook-Air.local>
2022-01-13 23:18:15 -0800

    TimersController
    1. Added if statement in create function that checks to see if a timer was created. If yes it will render a json: of the timer
    
    2. Created an index function that saves all created timers in a timers vazriable then renders json: timers
    
    3. Created a provate timer_params that takes in the input of :span, :desription, and :datetime

--- app/controllers/timers_controller.rb
+++ app/controllers/timers_controller.rb
@@ -3,10 +3,21 @@ class TimersController < ApplicationController
 
     def create 
         byebug
+        timer = Timer.new(timer_params)
+
+        if timer.save
+            render json: timer
+        end
     end
 
     def index
         timers = Timer.all
         render json: timers
+    end
+
+    private 
+
+    def timer_params
+        params.require(:timer).permit(:span, :description, :datetime)
     end
 end
